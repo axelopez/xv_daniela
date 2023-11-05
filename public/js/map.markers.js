d10aaf37ff6f9eadfc807dc0a525f3fdbd653a2f
Luis Cumatz <lcumatz@MacBook-Pro-2.local>
2023-11-04 18:24:21 -0600

    imagen joven

--- public/js/map.markers.js
+++ public/js/map.markers.js
@@ -1,17 +1,17 @@
 //List of map markers
 var map_markers = [
 	{
-		"title": "Evento",
-		"latitude": 14.5992629,
-		"longitude": -90.5113575,
+		"title": "Evento Religioso",
+		"latitude": 14.4042534,
+		"longitude": -90.6983809,
 		"icon":"fa-map-marker", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Lugar<br>Direccion"
+		"infoWindow":"Lugar: CATEDRAL SAN CRISTOBAL<br>Direccion: Calle Central 1ra Avenida, Zona 1, Palin, Escuintla"
 	} ,
   {
 		"title": "Recepcion",
-		"latitude":14.5992629,
-		"longitude":-90.5143575,
+		"latitude":14.401479094995866,
+		"longitude":-90.69317567611733,
 		"icon":"fa-birthday-cake", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Lugar<br>Direccion"
+		"infoWindow":"Lugar: SACRAMENTO GARDENS<br>Direccion: 5ta Calle 4 Avenida, Zona 2, Palin, Escuintla"
 	} 
 ];
